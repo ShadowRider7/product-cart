bb56c5c768679c420ef1ee0f1d79ad0c66c4eccd
MD. SAFAYET HOSSAIN SADHIN <sadhinv@gmail.com>
2026-02-16 15:12:35 +0600

    dynamic modal view ready

--- script/product.js
+++ script/product.js
@@ -40,7 +40,7 @@ const displayProducts = (products) => {
             </p>
             <h2 class="font-bold"><span>$</span>${product.price}</h2>
             <div class="flex justify-between gap-4 my-4">
-              <div class="btn btn-outline">
+              <div onclick="my_modal_5.showModal(); loadDetailedProduct(${product.id})" class="btn btn-outline">
                 <i class="fa-solid fa-eye"></i> Details
               </div>
               <div class="btn btn-outline">
@@ -52,6 +52,7 @@ const displayProducts = (products) => {
     cardContainer.append(card);
   });
 };
+
 const displayTopRatedProducts = (products) => {
   const trendingContainer = document.getElementById("trending");
   if (!trendingContainer) return;
@@ -82,7 +83,7 @@ const displayTopRatedProducts = (products) => {
             </p>
             <h2 class="font-bold"><span>$</span>${product.price}</h2>
             <div class="flex justify-between gap-4 my-4">
-              <div class="btn btn-outline">
+              <div onclick="my_modal_5.showModal()" class="btn btn-outline">
                 <i class="fa-solid fa-eye"></i> Details
               </div>
               <div class="btn btn-outline">
@@ -94,6 +95,60 @@ const displayTopRatedProducts = (products) => {
   `;
     trendingContainer.append(trendingProducts);
   });
+};
+
+const loadDetailedProduct = (id) => {
+  fetch(`https://fakestoreapi.com/products/${id}`)
+    .then((res) => res.json())
+    .then((data) => displayLoadedDetails(data));
+};
+// {
+//     "id": 1,
+//     "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+//     "price": 109.95,
+//     "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+//     "category": "men's clothing",
+//     "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
+//     "rating": {
+//         "rate": 3.9,
+//         "count": 120
+//     }
+// }
+const displayLoadedDetails = (product) => {
+  const modalView = document.getElementById("modalDetails");
+  modalView.innerHTML = `
+  <div class="card bg-base-100 shadow-sm">
+          <figure class="bg-blue-100">
+            <img
+              src="${product.image}"
+              class="h-60"
+            />
+          </figure>
+          <div class="m-3">
+            <div class="flex justify-between">
+              <h2 class="font-bold"><span>$</span>${product.price}</h2>
+              <div class="flex items-center gap-1">
+                <i class="fa-solid fa-star text-warning"></i>
+                <p>${product.rating.rate}(${product.rating.count})</p>
+              </div>
+            </div>
+            <h2 class="font-semibold text-2xl my-2">
+             ${product.title}
+            </h2>
+             <p>${product.description}</p>
+            <div class="flex justify-between gap-4 my-4">
+              <div class="btn btn-outline">
+                <i class="fa-solid fa-cart-shopping"></i> add
+              </div>
+              <div class="modal-action">
+            <form method="dialog">
+              <!-- if there is a button in form, it will close the modal -->
+              <button class="btn">Close</button>
+            </form>
+          </div>
+            </div>
+          </div>
+        </div> `;
 };
 const loadCategories = () => {
   const url = "https://fakestoreapi.com/products/categories";
